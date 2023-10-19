87e3ffafbe3016a39f28c4639d74259a4c6372a6
fazcholoth <falilkamali@gmail.com>
2023-10-19 12:12:22 +0530

    two

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    "origin": "http://localhost:3000",
+    "origin": "https://blogginapp.onrender.com",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
     "optionsSuccessStatus": 204
